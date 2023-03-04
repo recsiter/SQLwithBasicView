7d1e9b201eb6a5d6fea59a16ef83c78a99012a96
Gaborov <nagy.gabor70@gmail.com>
2023-03-04 14:50:09 +0100

    backup

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,6 +8,7 @@ import fundamentals.Fundamental;
 public class Main {
 
     public static void main(String[] args) {
+        //backup
         System.out.println("Omnem dimittite spem, o vos intrantes!");
         //you can use this to test your solutions
         Fundamental.testMySolutions();
