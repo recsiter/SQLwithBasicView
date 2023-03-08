1b34cd8b5fe44537da0a4594a4450c2b2607e437
recsiter <csomos.vince@gmail.com>
2023-03-08 11:36:28 +0100

    view készül1

--- src/view/EditFormDP.java
+++ src/view/EditFormDP.java
@@ -1,15 +1,15 @@
-package oop;
+package view;
 
 /**
  *
  * @author --G--
  */
-public class MainForm extends javax.swing.JFrame {
+public class EditFormDP extends javax.swing.JFrame {
 
     /**
-     * Creates new form MainForm
+     * Creates new form EditForm
      */
-    public MainForm() {
+    public EditFormDP() {
         initComponents();
     }
 
@@ -56,24 +56,27 @@ public class MainForm extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(MainForm.class.getName()).
+            java.util.logging.Logger.getLogger(EditFormDP.class.getName()).
                     log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(MainForm.class.getName()).
+            java.util.logging.Logger.getLogger(EditFormDP.class.getName()).
                     log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(MainForm.class.getName()).
+            java.util.logging.Logger.getLogger(EditFormDP.class.getName()).
                     log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(MainForm.class.getName()).
+            java.util.logging.Logger.getLogger(EditFormDP.class.getName()).
                     log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new MainForm().setVisible(true);
+                new EditFormDP().setVisible(true);
             }
         });
     }
